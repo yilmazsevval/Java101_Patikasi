92b3f077aea4f9952fd7c8885e71a6855c0ba785
yilmazsevval <110136552+yilmazsevval@users.noreply.github.com>
2023-05-25 17:19:27 +0300

    Add files via upload

---
+++ tersUcgen/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Basamak Sayisini Giriniz: ");
+        int number = input.nextInt();
+
+        for (int i =number; i >= 1; i--) {
+            for (int j = 1; j <= number - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= ((2 * i) - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
